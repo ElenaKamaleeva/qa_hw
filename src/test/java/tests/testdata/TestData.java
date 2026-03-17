e6ed6857b9bc662653a336c9d6b10b9ee749f606
Elena Kamaleeva <akinakaoin@mail.ru>
2026-03-17 19:04:18 +0200

    added testData

---
+++ src/test/java/tests/testdata/TestData.java
@@ -0,0 +1,30 @@
+package tests.testdata;
+
+public class TestData {
+
+    public static final String FIRST_NAME = "Elena";
+    public static final String LAST_NAME = "Kamaleeva";
+    public static final String EMAIL = "test_qa_guru@test.com";
+    public static final String PHONE = "4153456789";
+    public static final String PHONE_FORMATTED = "+1 415 345 6789";
+    public static final String GENDER = "Female";
+    public static final String DATE_OF_BIRTH_DAY = "05";
+    public static final String DATE_OF_BIRTH_MONTH = "05";
+    public static final String DATE_OF_BIRTH_YEAR = "1990";
+    public static final String DATE_OF_BIRTH_FORMATTED = "05/05/1990";
+    public static final String SUBJECT = "Maths";
+    public static final String LANGUAGE = "Russian";
+    public static final String HOBBY_1 = "Sports";
+    public static final String HOBBY_2 = "Reading";
+    public static final String HOBBY_3 = "Music";
+    public static final String HOBBIES_FORMATTED = "Sports, Reading, Music";
+    public static final String STATE = "California";
+    public static final String CITY = "San Diego";
+    public static final String STATE_CITY_FORMATTED = "California, San Diego";
+    public static final String ADDRESS = "Limassol, Cyprus";
+    public static final String FILE_NAME = "test.png";
+
+
+
+
+}
